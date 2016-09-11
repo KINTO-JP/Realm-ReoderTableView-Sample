a23ceabd116030cf9073bf068d7b0afa1b3db161
scrtree <M.kinoshita@gmail.com>
2016-09-11 22:05:20 +0900

    success build

--- Realm-ReoderTableView-Sample/FirstSampleRealmViewController.swift
+++ Realm-ReoderTableView-Sample/FirstSampleRealmViewController.swift
@@ -13,6 +13,7 @@ class FirstSampleRealmViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // aaa
         // Do any additional setup after loading the view, typically from a nib.
         
     }
