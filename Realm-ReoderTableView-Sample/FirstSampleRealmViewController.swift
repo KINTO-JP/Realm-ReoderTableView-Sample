17f79c85da5afdfedae8961f5f05abc71754e8e6
scrtree <M.kinoshita@gmail.com>
2016-09-12 23:50:12 +0900

    add constraints first-sample-realm-tableview

--- Realm-ReoderTableView-Sample/FirstSampleRealmViewController.swift
+++ Realm-ReoderTableView-Sample/FirstSampleRealmViewController.swift
@@ -8,19 +8,32 @@
 
 import UIKit
 
-class FirstSampleRealmViewController: UIViewController {
+class FirstSampleRealmViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var firstRealmTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // aaa
         // Do any additional setup after loading the view, typically from a nib.
+        
+        firstRealmTableView.delegate = self
+        firstRealmTableView.dataSource = self
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        cell?.textLabel?.text = "aaa"
+        
+        return cell!
     }
 
 }
